1316bc4e28148740c387918a2cc4d5ba9685628d
shlomii <shlomiizikovich@gmail.com>
2012-09-03 10:15:44 +0300

    Testing SQL Server Transport Performance

--- Samples/FullDuplex/MyServer/RequestDataMessageHandler.cs
+++ Samples/FullDuplex/MyServer/RequestDataMessageHandler.cs
@@ -8,10 +8,11 @@ namespace MyServer
 {
     public class RequestDataMessageHandler : IHandleMessages<RequestDataMessage>
     {
-        private const int Max = 10000;
+        private const int Max = 1000;
         private static bool first = true;
         private static readonly Stopwatch sw = new Stopwatch();
         private static int counter;
+        private static int counter2;
         public IBus Bus { get; set; }
 
         #region IHandleMessages<RequestDataMessage> Members
@@ -20,15 +21,20 @@ namespace MyServer
         {
             if (first)
             {
-                sw.Start();
-                first = false;
+                lock (typeof(int))
+                {
+                    sw.Start();
+                    first = false;
+                }
             }
             Interlocked.Increment(ref counter);
-            if ((counter >= Max))
+            Interlocked.Increment(ref counter2);
+            lock (typeof(int))
             {
-                Console.WriteLine(string.Format("Thread: [{0}] Time to receive: [{1}] messages is: [{2}]",
-                                                Thread.CurrentThread.ManagedThreadId, counter, sw.Elapsed.TotalSeconds));
-                Interlocked.Exchange(ref counter, 0);
+                if (counter <= Max) 
+                    return;
+                Console.WriteLine("Thread: [{0}] Time to receive: [{1}] messages is: [{2}], message counter is: [{3}]", Thread.CurrentThread.ManagedThreadId, counter, sw.Elapsed.TotalSeconds, counter2);
+                Interlocked.Exchange(ref counter, 0); 
                 sw.Restart();
             }
         }
